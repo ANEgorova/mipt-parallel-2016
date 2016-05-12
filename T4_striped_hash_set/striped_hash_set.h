4c5cfafcda563518f541af38888b99d72fc4bc29
ANEgorova <ladyae@ya.ru>
2016-05-12 10:15:39 +0300

    correct hashing  Please enter the commit message for your changes. Lines starting

--- T4_striped_hash_set/striped_hash_set.h
+++ T4_striped_hash_set/striped_hash_set.h
@@ -63,9 +63,9 @@ public:
    }
 
 
-   explicit striped_hash_set(std::size_t num_stripes, std::size_t growth_factor = 2, double coefficient = 0.5) :
-                                          num_stripes(num_stripes),
-                                          table_size(num_stripes * 35), growth_factor(growth_factor),
+   explicit striped_hash_set(std::size_t num_stripes_, std::size_t growth_factor_ = 2, double coefficient = 1) :
+                                          num_stripes(num_stripes_),
+                                          table_size(num_stripes * 35), growth_factor(growth_factor_),
                                           load_factor(0), lock_striping(num_stripes), coefficient(coefficient) {
 
         table.resize(table_size);
@@ -73,89 +73,94 @@ public:
 
 
     void remove(const Value& e){
-        lock_striping[(hash_function(e) % table_size) % num_stripes].write_lock();
-        std::size_t index = hash_function(e) % table_size;
+
+        lock_striping[stripe_index(e)].write_lock();
         if (belonging(e)){
-            auto it = std::find(table[index].begin(), table[index].end(), e);
-            table[index].remove(*it);
+            auto it = std::find(table[table_index(e)].begin(), table[table_index(e)].end(), e);
+            table[table_index(e)].remove(*it);
             amount--;
          }
 
-        lock_striping[(hash_function(e) % table_size) % num_stripes].write_unlock();
+        lock_striping[stripe_index(e)].write_unlock();
     }
 
     void add(const Value& e) {
+       lock_striping[stripe_index(e)].write_lock();
 
-
-          smb_resize.lock();
-
-          if (load_factor > coefficient) {
-
-                // lock all mutex
-                for (std::size_t i = 0; i < num_stripes; ++i) {
-                    lock_striping[i].write_lock();
-                }
-
-                // resize
-                table_size *= growth_factor;
-                table.resize(table_size);
-
-                // rehashing
-                for (std::size_t i = 0; i < table_size / growth_factor; ++i){
-                    for (auto it = table[i].begin(); it != table[i].end(); ++it){
-                        table[hash_function(e) % table_size].erase_after(it);
-                        table[hash_function(e) % table_size].push_front(e);
-                    }
-                }
-
-                for (std::size_t i = 0; i < num_stripes; ++i) {
-                    lock_striping[i].write_unlock();
-                }
-
-                get_load_factor();
-
-            }
-
-        smb_resize.unlock();
-
-        lock_striping[(hash_function(e) % table_size) % num_stripes].write_lock();
-        std::size_t index = hash_function(e) % table_size;
-
-        if (!belonging(e)) {
-            table[index].push_front(e);
+       if (!belonging(e)) {
+            table[table_index(e)].push_front(e);
             amount++;
-        }
+       }
 
-        lock_striping[index % num_stripes].write_unlock();
+       if (load_factor > coefficient) {
+          lock_striping[stripe_index(e)].write_unlock();
+          resize();
+       }
+       else lock_striping[stripe_index(e)].write_unlock();
     }
 
     bool contains(const Value& e) {
-        lock_striping[(hash_function(e) % table_size) % num_stripes].read_lock();
+        lock_striping[stripe_index(e)].read_lock();
         bool result;
 
         result = belonging(e);
 
-        lock_striping[(hash_function(e) % table_size) % num_stripes].read_unlock();
+        lock_striping[stripe_index(e)].read_unlock();
         return result;
     }
 
-    std::size_t get_load_factor(){
+    std::size_t set_load_factor(){
         load_factor =  amount / table_size;
         return load_factor;
     }
 
+    std::size_t table_index (const Value& e){
+        return hash_function(e) % table_size;
+    }
+
+    std::size_t stripe_index(const Value& e){
+        return table_index(e) % num_stripes;
+    }
+
+    void resize() {
+
+        if (amount > table_size * coefficient){
+            // resize
+            table_size.fetch_add(table_size*(growth_factor-1));
+            set_load_factor();
+
+            // lock all mutex
+            for (std::size_t i = 0; i < num_stripes; ++i) {
+                lock_striping[i].write_lock();
+            }
+
+            std::vector<std::forward_list<Value>> tmp_table(table_size);
+            // rehashing
+            for (std::size_t i = 0; i < table_size / growth_factor; ++i){
+                for (auto it = table[i].begin(); it != table[i].end(); ++it){
+                    tmp_table[table_index(*it)].push_front(*it);
+            }
+
+            table = tmp_table;
+            }
+
+            for (std::size_t i = 0; i < num_stripes; ++i) {
+                lock_striping[i].write_unlock();
+            }
+       }
+    }
+
     bool belonging(const Value& e) {
 
-        bool condition = std::find(table[hash_function(e) % table_size].begin(), table[hash_function(e) % table_size].end(), e) != table[hash_function(e) % table_size].end();
+        bool condition = std::find(table[table_index(e)].begin(), table[table_index(e)].end(), e) != table[table_index(e)].end();
 
         if (condition) return true;
-
         else return false;
     }
 
 private:
     std::size_t num_stripes;
-    std::size_t table_size;
+    std::atomic<std::size_t> table_size;
     std::size_t growth_factor;
     double load_factor;
     std::vector<RW_mutex> lock_striping;
@@ -163,5 +168,4 @@ private:
     std::atomic<std::size_t> amount;
     std::size_t coefficient;
     Hash hash_function;
-    std::mutex smb_resize;
 };
