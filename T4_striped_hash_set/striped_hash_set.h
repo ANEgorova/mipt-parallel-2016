3319f543939d57b7128f2303b136d6c7b2b9f8c7
ANEgorova <ladyae@ya.ru>
2016-05-16 01:16:01 +0300

    correct hash-table

--- T4_striped_hash_set/striped_hash_set.h
+++ T4_striped_hash_set/striped_hash_set.h
@@ -7,18 +7,19 @@
 #include <algorithm>
 #include <condition_variable>
 #include <atomic>
+#include <thread>
+#include <shared_mutex>
 
-class RW_mutex{
-
+class RW_mutex {
 public:
-    RW_mutex() : writers(0), readers(0), writing(false) {}
+   RW_mutex() : writers(0), readers(0), writing(false) {}
 
     void write_lock(){
         std::unique_lock<std::mutex> lock(gate);
-        writers++;
         while (writing || readers > 0){
-            room_empty.wait(lock);
+           room_empty.wait(lock);
         }
+        ++writers;
         writing = true;
     }
 
@@ -27,145 +28,144 @@ public:
         while (writers > 0){
             room_empty.wait(lock);
         }
-        readers++;
+        ++readers;
     }
 
     void write_unlock(){
         std::unique_lock<std::mutex> lock(gate);
-        writers--;
+        --writers;
         writing = false;
         lock.unlock();
         room_empty.notify_all();
     }
 
     void read_unlock(){
-        std::unique_lock<std::mutex> lock(gate, std::defer_lock);
-        lock.lock();
-        readers--;
-        if (readers == 0) room_empty.notify_one();
+        std::unique_lock<std::mutex> lock(gate);
+        --readers;
+        if (readers == 0) 
+            room_empty.notify_one();
     }
 
 private:
-    std::mutex gate;
-    std::size_t writers;
-    std::size_t readers;
-    std::condition_variable room_empty;
-    bool writing;
+   std::mutex gate;
+   std::size_t writers;
+   std::size_t readers;
+   std::condition_variable room_empty;
+   bool writing;
 };
 
 template <typename Value, class Hash = std::hash<Value>>
-
 class striped_hash_set {
 public:
 
-   striped_hash_set () : num_stripes(5), table_size(50), growth_factor(2), load_factor(0), lock_striping(5){
-        table.resize(table_size);
+   striped_hash_set () : num_stripes(5), growth_factor(2), lock_striping(5), amount(0){
+        table.resize(50);
    }
 
 
-   explicit striped_hash_set(std::size_t num_stripes_, std::size_t growth_factor_ = 2, double coefficient = 1) :
-                                          num_stripes(num_stripes_),
-                                          table_size(num_stripes * 35), growth_factor(growth_factor_),
-                                          load_factor(0), lock_striping(num_stripes), coefficient(coefficient) {
+   explicit striped_hash_set (std::size_t num_stripes_, std::size_t growth_factor_ = 2, std::size_t max_load_factor = 4) :
+                                          num_stripes(num_stripes_), growth_factor(growth_factor_),
+                                          lock_striping(num_stripes), amount(0), max_load_factor(max_load_factor) {
 
-        table.resize(table_size);
+        table.resize(num_stripes * 35);
     }
 
 
-    void remove(const Value& e){
+    void remove (const Value& e){
+        std::size_t stripe_index_ = stripe_index(e);
 
-        lock_striping[stripe_index(e)].write_lock();
+        lock_striping[stripe_index_].write_lock();
+
+        std::size_t table_index_ = table_index(e);
         if (belonging(e)){
-            auto it = std::find(table[table_index(e)].begin(), table[table_index(e)].end(), e);
-            table[table_index(e)].remove(*it);
+            auto it = std::find(table[table_index_].begin(), table[table_index_].end(), e);
+            table[table_index_].remove(*it);
             amount--;
          }
 
-        lock_striping[stripe_index(e)].write_unlock();
+        lock_striping[stripe_index_].write_unlock();
     }
 
-    void add(const Value& e) {
-       lock_striping[stripe_index(e)].write_lock();
+    void add (const Value& e) {
+        std::size_t stripe_index_ = stripe_index(e);
 
-       if (!belonging(e)) {
+        lock_striping[stripe_index_].write_lock();
+        if (!belonging(e)) {
             table[table_index(e)].push_front(e);
             amount++;
-       }
+        }
 
-       if (load_factor > coefficient) {
-          lock_striping[stripe_index(e)].write_unlock();
-          resize();
-       }
-       else lock_striping[stripe_index(e)].write_unlock();
+        if (get_load_factor() > max_load_factor) {
+            lock_striping[stripe_index_].write_unlock();
+            resize();
+        }
+
+        else lock_striping[stripe_index_].write_unlock();
     }
 
-    bool contains(const Value& e) {
-        lock_striping[stripe_index(e)].read_lock();
-        bool result;
+    bool contains (const Value& e) {
+        std::size_t stripe_index_ = stripe_index(e);
 
-        result = belonging(e);
+        lock_striping[stripe_index_].read_lock();
+        
+        bool result = belonging(e);
 
-        lock_striping[stripe_index(e)].read_unlock();
+        lock_striping[stripe_index_].read_unlock();
         return result;
     }
 
-    std::size_t set_load_factor(){
-        load_factor =  amount / table_size;
-        return load_factor;
-    }
+    void resize () {
 
-    std::size_t table_index (const Value& e){
-        return hash_function(e) % table_size;
-    }
+        // lock all mutex
+        for (std::size_t i = 0; i < num_stripes; ++i) {
+            lock_striping[i].write_lock();
+        }
 
-    std::size_t stripe_index(const Value& e){
-        return table_index(e) % num_stripes;
-    }
+        if (get_load_factor() > max_load_factor) {
 
-    void resize() {
-
-        if (amount > table_size * coefficient){
             // resize
-            table_size.fetch_add(table_size*(growth_factor-1));
-            set_load_factor();
+            std::vector<std::forward_list<Value>> tmp_table(table.size() * growth_factor);
 
-            // lock all mutex
-            for (std::size_t i = 0; i < num_stripes; ++i) {
-                lock_striping[i].write_lock();
-            }
-
-            std::vector<std::forward_list<Value>> tmp_table(table_size);
             // rehashing
-            for (std::size_t i = 0; i < table_size / growth_factor; ++i){
+            for (std::size_t i = 0; i < table.size(); ++i){
                 for (auto it = table[i].begin(); it != table[i].end(); ++it){
-                    tmp_table[table_index(*it)].push_front(*it);
+                    std::size_t table_index_ = hash_function(*it) % tmp_table.size();
+                    tmp_table[table_index_].push_front(*it);
+                }
             }
 
-            table = tmp_table;
-            }
+            table = std::move(tmp_table);
+        }
 
-            for (std::size_t i = 0; i < num_stripes; ++i) {
-                lock_striping[i].write_unlock();
-            }
-       }
-    }
-
-    bool belonging(const Value& e) {
-
-        bool condition = std::find(table[table_index(e)].begin(), table[table_index(e)].end(), e) != table[table_index(e)].end();
-
-        if (condition) return true;
-        else return false;
+        for (std::size_t i = 0; i < num_stripes; ++i) {
+            lock_striping[i].write_unlock();
+        }
     }
 
 private:
+
+    std::size_t get_load_factor () {
+        return amount / table.size();
+    }
+
+    std::size_t table_index (const Value& e) {
+        return hash_function(e) % table.size();
+    }
+
+    std::size_t stripe_index (const Value& e) {
+        return hash_function(e) % num_stripes;
+    }
+
+    bool belonging (const Value& e) {
+        std::size_t table_index_ = table_index(e);
+        return std::find(table[table_index_].begin(), table[table_index_].end(), e) != table[table_index_].end();
+    }
+
     std::size_t num_stripes;
-    std::atomic<std::size_t> table_size;
     std::size_t growth_factor;
-    double load_factor;
     std::vector<RW_mutex> lock_striping;
     std::vector<std::forward_list<Value>> table;
     std::atomic<std::size_t> amount;
-    std::size_t coefficient;
+    std::size_t max_load_factor;
     Hash hash_function;
 };
